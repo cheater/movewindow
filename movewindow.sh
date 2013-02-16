c534c41ccab5707ccfaaf93381580d3806702ff4
cheater <195390+cheater@users.noreply.github.com>
2013-02-16 16:34:18 +0100

    movewindow.sh: added the ability to break displays into "sub-displays"

--- movewindow.sh
+++ movewindow.sh
@@ -11,6 +11,9 @@
 
 # log="/tmp/dbglog" # dbg
 # echo "-----------" >> "$log" # dbg
+preferred_width=800
+max_width=1500
+
 
 wmctrl -r :ACTIVE: -b remove,maximized_horz
 wmctrl -r :ACTIVE: -b remove,maximized_vert
@@ -40,27 +43,66 @@ declare -a ranges
 declare -i monitor_num
 monitor_num=0
 
+declare -i vertical_offset
+declare -i width
+declare -i horiz_offset
+declare -i left
+declare -i right
+declare -i range_left
+declare -i range_right
+declare -i parts
+declare -i part
+declare -i start_
+declare -i end
+declare -i i
 # ranges is an array which contains entries of the form x1,x2;y where x1 is
 # the starting pixel column on the screen and x2 is the final pixel column and
 # y is the offset from top of desktop (i.e. starting pixel row). This is output
 # for every monitor.
 ranges=$(xrandr -q | awk '/ connected/{print $3}' | while read info; do
     # the output is: widthxheight+horizontal_offset+vertical_offset
-    declare -i vertical_offset
     vertical_offset=${info/*+/}
-    declare -i width
     width=${info/x*/}
     offsets=${info#*+}
-    declare -i horiz_offset
     horiz_offset=${offsets/+*/}
-    declare -i left
     left="$horiz_offset"
-    declare -i right
     right="$left + $width"
     echo "$left,$right;$vertical_offset"
-    done)
+    done | while read range_and_offset; do
+        range=${range_and_offset/;*/}
+        range_left=${range/,*/}
+        range_right=${range/*,/}
+        offset=${range_and_offset/*;/}
+        width="$range_right-$range_left"
 
-# echo ${ranges[@]} >> "$log" # dbg
+        echo width is "$width" >> "$log"
+        if [ "$width" -le "$max_width" ]; then
+            echo skipping "$range_and_offset" >> "$log"
+            echo "$range_and_offset"
+            continue
+            fi
+        echo not skipping "$range_and_offset" >> "$log"
+
+        parts="$width/$preferred_width" # $parts contains the amount of
+        # sub-monitors that will be created.
+        part="$width/$parts" # $part contains the number of pixel columns
+        # which will form one sub-monitor. Note one pixel might be missing.
+
+        for ((i=1; i<=$parts; i++)); do
+            start_="$range_left+($i-1)*$part"
+            if (($i == $parts)); then
+                # the last submonitor might have one pixel column missing
+                # because we are dividing in the integers, so let's account for
+                # the division remainder here.
+                end="$range_right"
+            else
+                end="$range_left+$i*$part"
+                fi
+            echo "$start_,$end;$offset"
+            done
+        done)
+
+echo ${ranges[@]} >> "$log" # dbg
 ranges_twice=$(
     for range_and_offset in ${ranges[@]}; do echo $range_and_offset; done
     for range_and_offset in ${ranges[@]}; do echo $range_and_offset; done
@@ -100,12 +142,13 @@ for range_and_offset in ${ranges[@]}; do
                 gravity=0
                 x="$range2_left"
                 y=${range_and_offset2/*;/}
-                w="$WIDTH"
+                width="$range2_right-$range2_left"
+                w="$width"
                 h="$HEIGHT"
                 # echo "$gravity,$x,$y,$w,$h" >> "$log" # dbg
                 wmctrl -r :ACTIVE: -e "$gravity,$x,$y,$w,$h"
 
-                wmctrl -r :ACTIVE: -b add,maximized_horz,maximized_vert
+                wmctrl -r :ACTIVE: -b add,maximized_vert
                 break
                 fi
             done
