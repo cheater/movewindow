9dbea0dc4d7021609ccfdbd6d33ad1241e0662ec
cheater <195390+cheater@users.noreply.github.com>
2018-04-08 23:39:02 +0200

    update code related to finding which sub-display we're on (or near) and moving to the next one

--- movewindow.sh
+++ movewindow.sh
@@ -74,29 +74,8 @@ else
 wmctrl -r :ACTIVE: -b remove,maximized_horz
 wmctrl -r :ACTIVE: -b remove,maximized_vert
 
-# Get the center of the window.
-eval "$(xdotool getactivewindow getwindowgeometry --shell)"
-# The above outputs something like:
-# WINDOW=70385876
-# X=593
-# Y=190
-# WIDTH=722
-# HEIGHT=422
-# SCREEN=0
-#
-# Do note, SCREEN has nothing to do with the monitor the window is being
-# displayed on. It is the X11 "screen", which is a concept defined in the
-# nomenclature of X11.
-
-declare -i window_left # left border
-window_left="$X"
-declare -i window_right
-window_right="$window_left + $WIDTH" # right border
-let "horiz_center=($window_left+$window_right)/2" # integer division
-
 # figure out which desktop the window falls on. we only need to use the widths
 # and horizontal offsets.
-declare -a columns
 declare -i monitor_num
 monitor_num=0
 
@@ -122,8 +101,10 @@ declare -i parts_with_overflow
 # finally f is 1 if the virtual column takes up the whole physical screen or
 # 0 otherwise.
 # This is put out for every virtual column.
-columns="$(echo "$monitor_info" | while IFS= read -r info; do
     # echo info is "$info" >> "$log" # dbg
+
+declare -a columns
+mapfile -t columns < <(echo "$monitor_info" | while IFS= read -r info; do
     # the output is: widthxheight+horizontal_offset+vertical_offset
     vertical_offset=${info/*+/}
     width=${info/x*/}
@@ -197,6 +178,7 @@ columns="$(echo "$monitor_info" | while IFS= read -r info; do
                 done
             done
         done)"
+        done)
 
 # echo columns are: ${columns[@]} >> "$log" # dbg
 # we need the columns plus one more, so if our window is at the last column,
@@ -204,70 +186,141 @@ columns="$(echo "$monitor_info" | while IFS= read -r info; do
 # the end of the list/array/whatever bash has.
 #
 # awk '!x[$0]++' is like uniq but doesn't require sorting.
-columns_extended="$(
-    for r in ${columns[@]}; do echo "$r"; done | awk '!x[$0]++'
-    for r in ${columns[@]}; do echo "$r"; break; done
-    )"
+declare -a columns_extended
+mapfile -t columns_extended < <(
+    for r in "${columns[@]}"; do echo "$r"; done | awk '!x[$0]++'
+    for r in "${columns[@]}"; do echo "$r"; break; done
+    )
 
-# search through the sub-displays to find the one we're on currently.
-for virtual_column in ${columns[@]}; do
+eval "$(xdotool getactivewindow getwindowgeometry --shell)"
+# The above outputs something like:
+# WINDOW=70385876
+# X=593
+# Y=190
+# WIDTH=722
+# HEIGHT=422
+# SCREEN=0
+#
+# Do note, SCREEN has nothing to do with the monitor the window is being
+# displayed on. It is the X11 "screen", which is a concept defined in the
+# nomenclature of X11.
+declare -i window_area
+window_area="$WIDTH*$HEIGHT"
+
+declare -i column_area
+declare -a columns_area_diff
+mapfile -t columns_area_diff < <(for virtual_column in ${columns[@]}; do
     range=${virtual_column%%;*}
-    # echo $range >> "range: $log" # dbg
     range_left=${range%%,*}
     range_right_and_height=${range#*,}
     range_right=${range_right_and_height%%,*}
     range_height=${range_right_and_height#*,}
-    if [ "$range_left" -le "$horiz_center" ]\
-    && [ "$horiz_center" -le "$range_right" ]; then
-        # echo "found in $virtual_column" >> "$log" # dbg
-        passed_current=0
-        # search through the sub-displays again for one after the one we are
-        # on currently; we also need to wrap around once.
-        for virtual_column2 in ${columns_extended[@]}; do
-            if [ "$virtual_column2" == "$virtual_column" ]; then
-                passed_current=1
-                continue
-                fi
-            if [ "$passed_current" -eq 1 ]\
-            && [ "$virtual_column2" != "$virtual_column" ]; then
-                range2=${virtual_column2%%;*}
-                range2_left=${range2%%,*}
-                # echo "found not in $virtual_column2" >> "$log" # dbg
-                range2_right_and_height=${range2#*,}
-                range2_right=${range2_right_and_height%%,*}
-                range2_height=${range2_right_and_height#*,}
-                eval "$(xdotool getactivewindow getwindowgeometry --shell)"
-                # the above outputs something like:
-                # WINDOW=70385876
-                # X=593
-                # Y=190
-                # WIDTH=722
-                # HEIGHT=422
-                # SCREEN=0
-                #
-                # do note, SCREEN has nothing to do with the monitor the window
-                # is being displayed on.
-                # dbg
-                # (echo 'xdotool output: '; xdotool getactivewindow getwindowgeometry --shell) >> "$log" # dbg
+    column_area="($range_right-$range_left)*$range_height"
+    d="$window_area-$column_area"
+    echo "sqrt(sqrt(($d)^2))" | bc
+    # We need to apply a second square root. Without it, we would have a result
+    # which is a difference of area in square units; however, later on, we need
+    # to create a score that is equally affected by the output of this, and the
+    # distance of window center to column center, which is in (non-square)
+    # units. To do this properly, both have to have the same dimension. So we
+    # either square root the result of this, or square the result of the other
+    # thing, but using larger numbers is likely worse, so i opted for square
+    # root here, because it resulted in smaller numbers.
+    done)
 
-                y_and_wholescreen=${virtual_column2#*;}
-                wholescreen=${y_and_wholescreen#*;}
-                gravity=0
-                x="$range2_left"
-                y=${y_and_wholescreen%%;*}
-                width="$range2_right-$range2_left-2*$border" # has something to
-                # do with 884/882 bug
-                w="$width"
-                h="$range2_height"
-                # echo "$gravity,$x,$y,$w,$h" >> "$log" # dbg
-                wmctrl -r :ACTIVE: -e "$gravity,$x,$y,$w,$h"
+# Get the center of the window.
+declare -i window_left # left border
+window_left="$X"
+declare -i window_right
+window_right="$window_left+$WIDTH" # right border
+declare -i window_center_horiz
+let "window_center_horiz=($window_left+$window_right)/2" # integer division
 
-                if [ "$wholescreen" -eq "1" ]; then
-                    wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz
-                    fi
+declare -i window_top
+window_top="$Y"
+declare -i window_bottom
+window_bottom="$window_top+$HEIGHT"
+declare -i window_center_vert
+let "window_center_vert=($window_top+$window_bottom)/2" # integer division
 
-                break
-                fi
-            done
-        fi
+declare -i column_center_horiz
+declare -i column_center_vert
+declare -a columns_center_diff
+mapfile -t columns_center_diff < <(for virtual_column in "${columns[@]}"; do
+    range=${virtual_column%%;*}
+    range_left=${range%%,*}
+    range_right_and_height=${range#*,}
+    range_right=${range_right_and_height%%,*}
+    range_height=${range_right_and_height#*,}
+    range_offset_and_maximized=${rangei#*;}
+    range_offset=${range_offset_and_maximized%%;*}
+    let "column_center_horiz=($range_left+$range_right)/2" # integer division
+    let "column_center_vert=$range_offset+$range_height/2" # integer division
+    diff_horiz="$column_center_horiz-$window_center_horiz"
+    diff_vert="$column_center_vert-$window_center_vert"
+    echo "sqrt(($diff_horiz)^2+($diff_vert)^2)" | bc
+    done)
+
+declare -i columns_len
+columns_amt=${#columns[@]}
+columns_len="$columns_amt-1"
+
+declare -a columns_score
+for i in $(seq 0 $columns_len); do
+    a=${columns_area_diff[$i]}
+    c=${columns_center_diff[$i]}
+    columns_score[$i]=$(echo "sqrt(($a)^2+($c)^2)" | bc)
     done
+
+declare -i columns_score_min
+columns_score_min=${columns_score[0]}
+for n in ${columns_score[@]}; do
+    if [ "$n" -lt "$columns_score_min" ]; then columns_score_min=$n; fi
+    done
+
+closest_column_idx="$(for i in $(seq 0 $columns_len); do
+    if [ "$columns_score_min" -eq "${columns_score[$i]}" ]; then
+        echo "$i"
+        break
+        fi
+    done)"
+
+# Jump to next virtual column
+
+declare -i closest_column_idx_next
+closest_column_idx_next="$closest_column_idx+1"
+virtual_column2="${columns_extended[$closest_column_idx_next]}"
+range2=${virtual_column2%%;*}
+range2_left=${range2%%,*}
+range2_right_and_height=${range2#*,}
+range2_right=${range2_right_and_height%%,*}
+range2_height=${range2_right_and_height#*,}
+eval "$(xdotool getactivewindow getwindowgeometry --shell)"
+# the above outputs something like:
+# WINDOW=70385876
+# X=593
+# Y=190
+# WIDTH=722
+# HEIGHT=422
+# SCREEN=0
+#
+# do note, SCREEN has nothing to do with the monitor the window
+# is being displayed on.
+# dbg
+# (echo 'xdotool output: '; xdotool getactivewindow getwindowgeometry --shell) >> "$log" # dbg
+
+y_and_wholescreen=${virtual_column2#*;}
+wholescreen=${y_and_wholescreen#*;}
+gravity=0
+x="$range2_left"
+y=${y_and_wholescreen%%;*}
+width="$range2_right-$range2_left-2*$border" # has something to
+# do with 884/882 bug
+w="$width"
+h="$range2_height"
+# echo "$gravity,$x,$y,$w,$h" >> "$log" # dbg
+wmctrl -r :ACTIVE: -e "$gravity,$x,$y,$w,$h"
+
+if [ "$wholescreen" -eq "1" ]; then
+    wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz
+    fi
